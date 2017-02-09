1a6ea86017e791d36dbe0b7243350a89f72dacd1
Juliana Arias <julianabea01@gmail.com>
2017-02-09 08:56:53 +0000

    4.6 links functions, style landing and product page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,8 @@
 class StaticPagesController < ApplicationController
   def landing_page
-    @products = Product.limit(4)
+    @products = Product.limit(5)
+  end
+
+  def about
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   resources :products
-  get 'static_pages/about'
+  get 'static_pages/about', to: 'static_pages#about', as:'about'
 
-  get 'static_pages/contact'
+  get 'static_pages/contact', to: 'static_pages#contact', as:'contact'
 
-  get 'static_pages/index'
+  get 'static_pages/index', to: 'static_pages#index', as:'index'
 
-  get 'static_pages/landing_page'
+  get 'static_pages/landing_page', to:'static_pages#landing_page', as:'landing_page'
 
   root 'static_pages#landing_page'
 
