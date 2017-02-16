d017e6e550bf5fe703b81e3bd4e6a175aecc429f
Juliana Arias <julianabea01@gmail.com>
2017-02-16 11:17:30 +0000

    5.3 contact form added

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,4 +5,17 @@ class StaticPagesController < ApplicationController
 
   def about
   end
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(:from => @email,
+      :to => 'julianabea01@gmail.com',
+      :subject => "A new contact form message from #{@name}",
+      :body => @message).deliver_now
+  end
+
 end
+
+
