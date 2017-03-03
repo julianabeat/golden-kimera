202e8294c85ddb2d5a5ba430ed2270b880451c6a
Juliana Arias <julianabea01@gmail.com>
2017-03-03 21:13:03 +0000

    5.8 ratings and comments added to products

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+
+  resources :products do
+    resources :comments
+  end
+
   resources :users
-  resources :products
   
   get 'static_pages/about', to: 'static_pages#about', as:'about'
 
@@ -16,6 +20,8 @@ Rails.application.routes.draw do
   root 'static_pages#landing_page'
 
   resources :orders, only: [:index, :show, :create, :destroy]
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
