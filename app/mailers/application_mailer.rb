a9c57c41171103356ee21d86ac87842f2131c6d5
Juliana Arias <julianabea01@gmail.com>
2017-03-13 10:15:56 +0000

    5.11 check mailer form

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'bikes4life01@example.com'
+  default from: 'julianabea01@gmail.com'
   layout 'mailer'
 end
--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,6 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
   def contact_form
-    UserMailer.contact_form("bikes4life@example.com", "Bikes4life", "Your message is very important for us, here some links for you: We <3 Bikes4life!")
+    UserMailer.contact_form("julianabea01@gmail.com", "Bikes4life", "Your message is very important for us, here some links for you: We <3 Bikes4life!")
   end
 end
