5d3d9f328435f735341e521803dd25dd548605dc
Juliana Arias <julianabea01@gmail.com>
2017-03-13 15:35:58 +0000

    5.11 Automated Email added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}, :controllers => { :registrations => "user_registrations" }
 
   resources :products do
     resources :comments
@@ -20,8 +20,6 @@ Rails.application.routes.draw do
   root 'static_pages#landing_page'
 
   resources :orders, only: [:index, :show, :create, :destroy]
-
-
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
