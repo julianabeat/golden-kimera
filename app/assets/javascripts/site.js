e7b76f5c1050282d6dfb45c52f48d0c9f7e4e59d
Juliana Arias <julianabea01@gmail.com>
2017-06-29 00:15:12 +0100

    add ajax to comments

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,12 +1,25 @@
 $(document).on('turbolinks:load', function(){
 
-    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-    $('.rated').raty({ path: '/assets',
-      readOnly: true,
-      score: function() {
-        return $(this).attr('data-score');
-      }
-    });
+  var refreshRating = function() {
+  $('.rating').raty({
+    starHalf: '<%= asset_path 'star-half.png' %>',
+    starOff: '<%= asset_path 'star-off.png' %>',
+    starOn: '<%= asset_path 'star-on.png' %>',
+    scoreName: 'comment[rating]'
+  });
+    $('.rated').raty({
+    starHalf: '<%= asset_path 'star-half.png' %>',
+    starOff: '<%= asset_path 'star-off.png' %>',
+    starOn: '<%= asset_path 'star-on.png' %>',
+    readOnly: true,
+    score: function() {
+      return $(this).attr('data-score');
+    }
+  });
+};
+
+$(document).on('turbolinks:load', function() {
+    refreshRating();
 
     $('.img-zoom').elevateZoom({
     zoomType: "lens",
